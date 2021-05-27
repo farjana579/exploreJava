e3c04e6f349311231b984c6cb2ea0ea0e3fdae45
farjanaabedin <farjanaabedin579@gmail.com>
2021-05-27 11:13:17 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,76 @@
+var bananaPrice=12;
+console.log(bananaPrice);
+console.log(typeof bananaPrice);
+var name="Farjana abedin";
+console.log(name);
+console.log(typeof name);
+var isHot=true;
+var isRich=false;
+console.log(isHot);
+console.log(isRich);
+console.log(typeof isRich);
+console.log(typeof isHot);
+var promise= "i promise i wil try my best to SPEAK english";
+console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+console.log(promise.indexOf("try"));
+console.log(promise.split('try'));
+console.log(promise.indexOf("speak"));
+var number1= 25;
+var number2=15.5;
+console.log(number1+number2);
+var number2='15.5';
+console.log(number1+number2);
+number2=parseFloat(number2);
+console.log(number2+number1);
+number2=parseInt(number2);
+console.log(number2+number1);
+var number3=34;
+number3=''+number3;
+console.log(typeof number3);
+number2='15.5';
+number2=+number2;
+console.log(typeof number2);
+var num1=-5
+console.log(Math.abs(num1));
+var num2=5;
+console.log(Math.abs(num2));
+var num=5.59999;
+console.log(Math.round(num))
+var number= 5.4999;
+console.log(Math.round(number))
+num=4.9999
+console.log(Math.ceil(num))
+console.log(Math.floor(num))
+console.log(Math.random())
+var number=Math.random()*200
+var result=Math.round(number)
+console.log(result)
+var date= new Date('1971-12-16');
+console.log(date);
+date=new Date('2000-12-07')
+console.log(date);
+var name="banana";
+var price=12;
+if(name=="banana" && price==12)
+{
+    console.log("yes");
+}
+else{
+    console.log("nop");
+} 
+ var name="banana";
+var price= 50
+if(name!="banana" || price!=50)
+{
+    console.log("yes");
+}      
+else if(name=='banana' && price==50)
+{
+    console.log('maybe');
+}
+else{
+    console.log('no');
+}
+    
+
